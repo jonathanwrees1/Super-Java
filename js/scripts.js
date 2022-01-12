6272ef47c9f8febefe7a3fce59eac94f737b7813
jonathanwrees1 <97190782+jonathanwrees1@users.noreply.github.com>
2022-01-11 22:16:30 -0800

    Changed Charmander type key to an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList=[
     {myName:'Bulbasaur', height:7, type:['grass','poison']},
-    {myName:'Charmander', height:6, type:'fire.'},
+    {myName:'Charmander', height:6, type:['fire.']},
     {myName:'Beedrill', height:1, type:['bug','poison']},
 ];

8e9c85fee7890a9f1bbe4e04075ce18030f28f7f
jonathanwrees1 <97190782+jonathanwrees1@users.noreply.github.com>
2022-01-12 21:04:19 -0800

    Created loop with if/then/else statement. Document.write to DOM. Some CSS styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
-let pokemonList=[
-    {myName:'Bulbasaur', height:7, type:['grass','poison']},
-    {myName:'Charmander', height:6, type:['fire.']},
-    {myName:'Beedrill', height:1, type:['bug','poison']},
+let pokemonList = [
+  { myName: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+
+  { myName: 'Charmander', height: 0.6, type: ['fire.'] },
+
+  { myName: 'Beedrill', height: 1.0, type: ['bug', 'poison'] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 0.7) {
+    document.write(
+      pokemonList[i].myName +
+        ' (height: ' +
+        pokemonList[i].height +
+        'm) - ' +
+        'Wow, that\'s big!' +
+        '<br /><br />'
+    );
+  } else
+    document.write(
+      pokemonList[i].myName +
+        ' (height: ' +
+        pokemonList[i].height +
+        'm)' +
+        '<br /><br />'
+    );
+}

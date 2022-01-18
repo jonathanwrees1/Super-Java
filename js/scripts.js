67ef1e1082096225bd26198ccf9075a81439d95c
jonathanwrees1 <97190782+jonathanwrees1@users.noreply.github.com>
2022-01-17 20:16:00 -0800

    Included IIFE functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,41 @@
-let pokemonList = [
-  { myName: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { myName: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
 
-  { myName: 'Charmander', height: 0.6, type: ['fire.'] },
+    { myName: 'Charmander', height: 0.6, type: ['fire.'] },
 
-  { myName: 'Beedrill', height: 1.0, type: ['bug', 'poison'] },
-];
-pokemonList.forEach (function(trait) {
+    { myName: 'Beedrill', height: 1.0, type: ['bug', 'poison'] },
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+
+  };
+
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add('Golom');
+console.log(pokemonRepository.getAll());
+
+
+(pokemonRepository.getAll()).forEach(function (trait) {
   if (trait.height > 0.7) {
     document.write(
       trait.myName +
-        ' (height: ' +
-        trait.height +
-        'm) - ' +
-        'Wow, that\'s big!' +
-        '<br /><br />'
+      ' (height: ' +
+      trait.height +
+      'm) - ' +
+      "Wow, that's big!" +
+      '<br /><br />'
     );
-  } else
-    document.write(
-      trait.myName +
-        ' (height: ' +
-        trait.height +
-        'm)' +
-        '<br /><br />'
-    );
+  } else document.write(trait.myName + ' (height: ' + trait.height + 'm)' + '<br /><br />');
 });
-
-
